7dfbb6b2f87e7d6d2fb033f113eecb4db92e2a38
alzahedi <123923192+alzahedi@users.noreply.github.com>
2025-03-09 13:58:14 +0530

    Update download-and-extract.ps1

--- scripts/download-and-extract.ps1
+++ scripts/download-and-extract.ps1
@@ -6,11 +6,17 @@ $scriptPath = "C:\Scripts"
 New-Item -ItemType Directory -Path $scriptPath -Force | Out-Null
 
 $resource = "https://storage.azure.com/"
-$token = (Invoke-RestMethod -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=$resource" -Headers @{"Metadata"="true"} -Method GET).access_token
+$token = (Invoke-RestMethod -Method Get -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=$resource" -Headers @{Metadata="true"}).access_token
+
+# Set up headers for blob access
+$headers = @{
+        Authorization = "Bearer $token"
+        "x-ms-version" = "2019-02-02"
+}
 
 $zipFilePath = "$scriptPath\scripts.zip"
 $extractPath = "$scriptPath\extracted"
 
-Invoke-WebRequest -Uri $blobStorageUrl -Headers @{Authorization = "Bearer $token"} -OutFile $zipFilePath
+Invoke-WebRequest -Uri $blobStorageUrl -Headers $headers -OutFile $zipFilePath
 Expand-Archive -Path $zipFilePath -DestinationPath $extractPath -Force
 Write-Host "Script execution completed. Files extracted to: $extractPath"
