0dc5b95c92dc77b92b6abdcc360a0d6e39bff3a7
alzahedi <123923192+alzahedi@users.noreply.github.com>
2025-03-10 12:25:07 +0530

    Update setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,35 +1,4 @@
-Write-Output "Starting Python installation..."
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-# Use Invoke-WebRequest with better error handling
-$chocoInstallScript = "https://community.chocolatey.org/install.ps1"
-Invoke-WebRequest -Uri $chocoInstallScript -UseBasicParsing -OutFile install.ps1
-
-if (Test-Path "install.ps1") {
-    Write-Output "Chocolatey script downloaded successfully"
-    Invoke-Expression -Command (Get-Content install.ps1 -Raw)  # Run script inline
-} else {
-    Write-Output "Failed to download Chocolatey script"
-    exit 1
-}
-
-# Add Chocolatey to the PATH
-$env:Path += ";C:\ProgramData\chocolatey\bin"
-
-# Ensure Chocolatey is installed before proceeding
-if (!(Test-Path "C:\ProgramData\chocolatey\bin\choco.exe")) {
-    Write-Output "Chocolatey installation failed."
-    exit 1
-}
-
 # Install Python
-choco install -y python310 --no-progress --limit-output
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "python_installer.exe"
+Start-Process -FilePath "python_installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
 
-if ($?) {
-    Write-Output "Python installed successfully"
-} else {
-    Write-Output "Python installation failed"
-    exit 1
-}
-
-Write-Output "Finished"
