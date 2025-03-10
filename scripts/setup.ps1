1d24108df9a222f262d77dd3741a6f61f25c95db
alzahedi <123923192+alzahedi@users.noreply.github.com>
2025-03-10 12:41:42 +0530

    Update setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,4 +1,1 @@
-# Install Python
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe" -OutFile "python_installer.exe"
-Start-Process -FilePath "python_installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-
+Write-Host "hello World"
